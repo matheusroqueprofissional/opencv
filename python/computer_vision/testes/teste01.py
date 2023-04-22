06010c8b2055006f7b63d0f67fe4c9524781ce5f
matheusroqueprofissional <matheus.roqueprofissional@gmail.com>
2023-04-21 23:32:36 -0300

    projeto de teste finalizado

--- python/computer_vision/testes/teste01.py
+++ python/computer_vision/testes/teste01.py
@@ -3,26 +3,28 @@ import numpy as np
 print("nesse codigo iremos tentar desenvolver um codigo capaz de identificar\n",
        "um rosto humano e desenhar linhas no rosto e nos olhos")
 
-img =cv.imread("../../../images/rosto_negro01.jfif")
+img =cv.imread("../../../images/grupodepessoas02.jpg")
 gray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
 shape = img.shape
 tamanho = (600,600)
 resizeimage = cv.resize(img,tamanho,interpolation=cv.INTER_AREA)
 df = cv.CascadeClassifier("../../../xml/frontalface.xml")
 ey = cv.CascadeClassifier("../../../xml/haarcascade_eye.xml")
-faces = df.detectMultiScale(resizeimage,scaleFactor = 1.05, minNeighbors= 7,
-                            minSize = (30,30), flags=cv.CASCADE_SCALE_IMAGE)
+faces = df.detectMultiScale(resizeimage,scaleFactor = 1.05, minNeighbors= 5,
+                            minSize = (20,20), flags=cv.CASCADE_SCALE_IMAGE)
 
-contador = 0
-
+contadorface = 0
+contadoreyes = 0
+kernel = (3,3)
 for (x,y,w,h) in faces:
-    contador= contador+1
+    contadorface= contadorface+1
     cv.rectangle(resizeimage,(x,y),(x+w,+y+h),(0,0,255),1)
     roi_gray = gray[y:y+h, x:x+w]
     roi_color = resizeimage[y:y+h, x:x+w]
     eyes = ey.detectMultiScale(roi_color, scaleFactor=1.07, minNeighbors=7, minSize=(50, 50), maxSize=(100, 100))
 
     for(ex,ey,ew,eh) in eyes:
+        contadoreyes = contadoreyes+1
         cv.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(255,0,0),2)
 
 #0 altura
@@ -30,3 +32,13 @@ for (x,y,w,h) in faces:
 print(shape)
 cv.imshow("janela",resizeimage)
 cv.waitKey(0)
+import pyttsx3
+
+# Cria um objeto do motor de voz
+engine = pyttsx3.init()
+
+
+
+# Faz o Python falar a frase
+engine.say(f"a imagem tem {contadorface} faces e {contadoreyes} olhos possiveis de detectar")
+engine.runAndWait()
